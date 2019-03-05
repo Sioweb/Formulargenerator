4c2deb764fec04a732c22912d92cdb4bbe221281
Sascha Weidner <=>
2019-03-05 11:01:15 +0100

    Change type if validation is set

--- src/Fields/Text.php
+++ src/Fields/Text.php
@@ -2,6 +2,19 @@
 
 namespace Sioweb\Lib\Formgenerator\Fields;
 
+use Sioweb\Lib\Formgenerator\Core\Form;
+
 class Text extends Field
 {
+    public function __construct($fieldId, array $FieldConfig, Form $form)
+    {
+        parent::__construct($fieldId, $FieldConfig, $form);
+        if(!empty($this->validation)) {
+            switch(strtolower($this->validation)) {
+                case 'email':
+                    $this->type = 'email';
+                    break;
+            }
+        }
+    }
 }
