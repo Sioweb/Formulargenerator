3d591bc9025055eaac6dd74dc07898563cdd477b
Sascha Weidner <=>
2019-03-06 11:12:54 +0100

    Walk options for all fieldtypes

--- src/Fields/Checkbox.php
+++ src/Fields/Checkbox.php
@@ -1,8 +1,39 @@
 <?php
 
+declare (strict_types = 1);
+
 namespace Sioweb\Lib\Formgenerator\Fields;
+
+use Sioweb\Lib\Formgenerator\Attributes\Options;
+use Sioweb\Lib\Formgenerator\Core\Form;
 
 class Checkbox extends Field
 {
+    public function __construct($fieldId, array $FieldConfig, Form $Form)
+    {
+        parent::__construct($fieldId, $FieldConfig, $Form);
+        $Options = new Options($this);
+        $FieldOptions = $Options->getValue();
+        if(!empty($FieldOptions)) {
+            $this->value = $FieldOptions;
+            $this->name .= '[]';
+        }
+    }
 
+    public function getValue()
+    {
+        if (is_array($this->value)) {
+            $Values = [];
+            foreach ($this->value as $key => $option) {
+                if (!empty($option['active'])) {
+                    $Values[] = $option['key'];
+                }
+            }
+            return $Values;
+        } else {
+            return $this->value;
+        }
+
+        return false;
+    }
 }
--- src/Fields/Radio.php
+++ src/Fields/Radio.php
@@ -1,7 +1,33 @@
 <?php
 
+declare (strict_types = 1);
+
 namespace Sioweb\Lib\Formgenerator\Fields;
+
+use Sioweb\Lib\Formgenerator\Attributes\Options;
+use Sioweb\Lib\Formgenerator\Core\Form;
 
 class Radio extends Field
 {
+    public function __construct($fieldId, array $FieldConfig, Form $Form)
+    {
+        parent::__construct($fieldId, $FieldConfig, $Form);
+        $Options = new Options($this);
+        $this->value = $Options->getValue();
+    }
+
+    public function getValue()
+    {
+        if (is_array($this->value)) {
+            foreach ($this->value as $key => $option) {
+                if (!empty($option['active'])) {
+                    return $option['key'];
+                }
+            }
+        } else {
+            return $this->value;
+        }
+
+        return false;
+    }
 }
